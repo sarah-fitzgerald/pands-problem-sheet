aa62faca30f6ef5240cacf60349cb38d0fce3aa7
unknown <G00398363@gmit.ie>
2021-02-20 21:00:36 +0000

    test

---
+++ secondstring.py
@@ -0,0 +1,8 @@
+#This program takes user string inputs and outputs every second letter in reverse order
+#Author: Sarah Fitzgerald
+
+#Asks user to input a sentence as a string
+sentence = str(input( "Please enter a sentence: " )) [::-1] #[::-1} reverses the sentence - https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+#Prints out every second letter of the sentence in reverse order
+print (sentence[::2]) #[::2] prints our every second letter - https://stackoverflow.com/questions/20847205/program-to-extract-every-alternate-letters-from-a-string-in-python/20847220
